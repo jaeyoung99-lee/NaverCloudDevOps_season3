992d3df88b95e53a92f5ad643593dc5187075761
jaeyoung <leejy0715@naver.com>
2024-01-18 19:51:35 +0900

    구글맵에서 지도 HTML 주소 가져오는 법

--- reactwork/day0118router/src/pages/HomeSub3.js
+++ reactwork/day0118router/src/pages/HomeSub3.js
@@ -6,7 +6,10 @@ const HomeSub3 = () => {
         <div>
              <Alert variant='filled' severity='primary'>
                  오시는 길
-            </Alert> 
+            </Alert>
+
+             {/* 지도 HTML 퍼오는 법 */}
+             {/* 구글맵 -> 위치 검색 -> 해당 위치 선택 후 공유 클릭 -> 지도 퍼가기 -> HTML 복사 */}
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25321.34095631755!2d126.98406277773302!3d37.503964590407776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca15aee9ab0cb%3A0x31793fc63e0cf9d3!2z67mE7Yq47Lqg7ZSE!5e0!3m2!1sko!2skr!4v1705562519647!5m2!1sko!2skr"
              width="600" height="450" style={{border:0}}
              allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
