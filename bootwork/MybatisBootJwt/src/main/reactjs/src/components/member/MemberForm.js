774851ea49fd30485c8c57df3b7c587a7ed40ab0
jaeyoung <leejy0715@naver.com>
2024-01-24 10:31:06 +0900

    카카오 주소록 따오기

--- bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberForm.js
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberForm.js
@@ -2,10 +2,38 @@ import { CameraAltOutlined } from '@mui/icons-material';
 import axios from 'axios';
 import React, { useState } from 'react';
 import errimg from '../../image/noimage.jpg';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DaumPostcodeEmbed from 'react-daum-postcode';
 
 const MemberForm = () => {
     const [photo, setPhoto] = useState('');
     const [addr, setAddr] = useState('');
+    const [open, setOpen] = useState(false); // 다이얼로그 open/close
+    const [openPostcode, setOpenPostcode] = useState(false); // 카카오 주소록 open/close
+
+    const handleClickOpen = () => {
+        setOpen(true);
+        setOpenPostcode(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setOpenPostcode(false);
+    };
+
+    // 주소 선택 완료 시 호출될 이벤트
+    const selectAddress = (data) => {
+        console.dir(data); // dir로 하면 log보다 디테일하게 나옴
+        setAddr(`(${data.zonecode}) ${data.address} ${data.buildingName}`);
+
+        // 주소 선택 시 출력 후 카카오 주소록과 다이얼로그를 닫는다.
+        handleClose();
+    }
 
     // 네이버 스토리지의 이미지 폴더명
     const imageUrl = process.env.REACT_APP_STORAGE;
@@ -26,6 +54,36 @@ const MemberForm = () => {
 
     return (
         <div>
+            {/* 카카오 주소록을 보기 위한 다이얼로그 */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                {"카카오 주소록"}
+                </DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    {
+                        // 카카오 주소창
+                        openPostcode &&
+                        <DaumPostcodeEmbed
+                            onComplete={selectAddress} // 값을 선택할 경우 실행되는 이벤트
+                            autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
+                            defaultQuery='강남대로 24길' // 팝업을 열 때 검색창의 기본 주소
+                        />
+                    }
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose}>
+                    Close
+                </Button>
+                </DialogActions>
+            </Dialog>
+            
             <h4 className='alert alert-danger'>회원 가입</h4>
             <table className='table table-bordered' style={{width: '530px'}}>
                 <tbody>
@@ -68,8 +126,12 @@ const MemberForm = () => {
                     <tr>
                         <td colSpan={3}>
                             <div className='input-group'>
-                                <input type='text' className='form-control' style={{width: '400px'}}/>
-                                <button type='button' className='btn btn-sm btn-secondary'>주소 검색</button>
+                                <input type='text' className='form-control' style={{width: '400px'}} value={addr}
+                                onChange={(e) => setAddr(e.target.value)}/>
+                                <button type='button' className='btn btn-sm btn-secondary'
+                                onClick={handleClickOpen}>
+                                    주소 검색
+                                </button>
                             </div>
                         </td>
                     </tr>
